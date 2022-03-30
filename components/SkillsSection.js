a3b66f786ddc0a24029d8a9b61313d635811cf95
HassanKhan96 <khanhassan057@gmail.com>
2022-03-31 00:45:48 +0500

    changes-in-skillbox

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -1,34 +1,32 @@
-import { SiReact, SiExpress, SiMongodb } from "react-icons/si";
+import { SiReact, SiExpress, SiMongodb, SiNodedotjs, SiNextdotjs, SiNestjs } from "react-icons/si";
 import { DiNodejs } from "react-icons/di";
 import SkillBox from "./skill/SkillBox";
 
 const SkillSection = () => {
+  const iconSize = 26
   return (
     <div className="container-fluid mx-auto h-screen p-5 md:p-10">
       <div className="font-poppins text-2xl md:text-4xl font-semibold text-black">
         Skills
       </div>
       <div className="flex flex-col sm:flex-col-reverse md:flex-row md:justify-evenly mt-10 md:mt-20">
-        <div className="md:w-3/5 mt-5">
-          {/* <div className="grid sm:grid-cols-2 md:grid-cols-4 justify-center text-primary"> */}
-          <div className="flex gap-x-2 gap-y-4 flex-wrap text-primary">
-            <SkillBox name="React" icon={<SiReact size={20} />} rating={7} />
-            <SkillBox name="React" icon={<SiReact size={20} />} rating={7} />
-            <SkillBox name="React" icon={<SiReact size={20} />} rating={7} />
-            <SkillBox name="React" icon={<SiReact size={20} />} rating={7} />
-            <SkillBox name="React" icon={<SiReact size={20} />} rating={7} />
-            <SkillBox name="React" icon={<SiReact size={20} />} rating={7} />
-            <SkillBox name="React" icon={<SiReact size={20} />} rating={7} />
-            <SkillBox name="React" icon={<SiReact size={20} />} rating={7} />
-            <SkillBox name="React" icon={<SiReact size={20} />} rating={7} />
-            <SkillBox name="React" icon={<SiReact size={20} />} rating={7} />
-          </div>
-        </div>
         <div className="md:w-2/5 flex justify-center">
           <div className="w-20 md:w-40">
             <img src="/skill_graphics.svg" className="w-full h-full" />
           </div>
         </div>
+        <div className="md:w-3/5 mt-5">
+          {/* <div className="grid sm:grid-cols-2 md:grid-cols-4 justify-center text-primary"> */}
+          <div className="flex gap-5 flex-wrap justify-center md:justify-start text-primary">
+            <SkillBox name="React JS" icon={<SiReact size={iconSize} />} rating={10} />
+            <SkillBox name="React Native" icon={<SiReact size={iconSize} />} rating={7} />
+            <SkillBox name="Node JS" icon={<SiNodedotjs size={iconSize} />} rating={7} />
+            <SkillBox name="Express JS" icon={<SiExpress size={iconSize} />} rating={7} />
+            <SkillBox name="Nest JS" icon={<SiNestjs size={iconSize} />} rating={7} />
+            <SkillBox name="Next JS" icon={<SiNextdotjs size={iconSize} />} rating={7} />
+          </div>
+        </div>
+
       </div>
     </div>
   );
--- components/skill/SkillBox.js
+++ components/skill/SkillBox.js
@@ -1,43 +1,53 @@
 import { SiReact, SiExpress, SiMongodb } from "react-icons/si";
-import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 
-const SkillBox = ({ icon = "", name = "", rating = 0 }) => {
-  let fullStar = rating / 2;
-  let halfStar = rating % 2;
 
-  return (
-    <div className="w-32 h-40 md:w-24 md:h-32 flex flex-col bg-primary border-0 p-0 relative before:content-[''] before:absolute before:w-0 before:h-0 before:top-0 before:right-0 before:border-r-[2rem] before:border-r-bgColor before:border-b-[2rem] before:border-b-primaryDark before:shadow-[-1px_2px_2px_-5px_rgba(0,0,0)]">
-      <div className="text-white w-full m-3">{icon}</div>
-      <div className="m-3">
-        <div className="text-white text-xl">{name}</div>
-        <div className="text-white flex mt-2">
-          {[...Array(5)].map((star, i) => {
-            if (i + 1 <= fullStar) {
-              return (
-                <BsStarFill
-                  key={i + Math.random()}
-                  size={20}
-                  className="mr-1"
-                />
-              );
-            } else if (i + 1 > fullStar && halfStar !== 0) {
-              return (
-                <BsStarHalf
-                  key={i + Math.random()}
-                  size={20}
-                  className="mr-1"
-                />
-              );
-            } else {
-              return (
-                <BsStar key={i + Math.random()} size={20} className="mr-1" />
-              );
+const SkillBox = ({ icon = '', name = '', rating = 0 }) => {
+
+    const renderStar = () => {
+        rating /= 2;
+        let stars = []
+        for (let i = 1; i <= 5; i++) {
+            if (rating === 0.5) {
+                stars.push(<BsStarHalf key={i + Math.random()} size={16} />)
+                rating -= 0.5
             }
-          })}
+            else if (rating > 0.5) {
+                stars.push(<BsStarFill key={i + Math.random()} size={16} />)
+                rating--
+            }
+            else {
+                stars.push(<BsStar key={i + Math.random()} size={16} />)
+            }
+        }
+        return stars
+    }
+
+    return <div className="w-28 h-[9rem] flex flex-col bg-primary shadow-[-2px_2px_2px_-2px_rgba(0,0,0)] relative 
+    before:content-[''] before:absolute before:w-0 before:h-0 before:top-0 before:right-0 before:border-r-[2.3rem] before:border-r-bgColor before:border-b-[2.3rem] before:border-b-primary before:shadow-[-2px_2px_4px_-2px_rgba(77,77,138,0.9)]">
+        <div className="text-white mt-3 ml-2">
+            {icon}
         </div>
-      </div>
+        <div className="flex-auto flex flex-col">
+            <div className="text-white flex-auto flex justify-start items-end pb-3 flex-wrap pl-2">{name}</div>
+            <div className="text-primaryDark flex flex-wrap gap-[4px] mb-5 justify-center">
+                {
+                    renderStar()
+                    // [...Array(5)].map((star, i) => {
+                    //     if ((i + 1) <= fullStar) {
+                    //         return <BsStarFill key={i + Math.random()} size={16} />
+                    //     }
+                    //     else if ((i + 1) > fullStar && halfStar !== 0) {
+                    //         return <BsStarHalf key={i + Math.random()} size={16} />
+                    //     }
+                    //     else {
+                    //         return <BsStar key={i + Math.random()} size={16} />
+                    //     }
+                    // })
+                }
+            </div>
+        </div>
     </div>
-  );
-};
+}
 
 export default SkillBox;
