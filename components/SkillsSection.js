150176b9561fbfae7ba7f4476f42f6e4f0bb42b4
Muhammad Hassan <muhammad.hassan@celeritasdigital.com>
2022-04-01 19:46:48 +0500

    changes

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
 import {
   SiReact,
   SiExpress,
@@ -12,97 +14,151 @@ import {
   SiMysql,
 } from "react-icons/si";
 import SkillBox from "./skill/SkillBox";
+import styles from "../styles/Animation.module.css";
 
 const SkillSection = () => {
   const iconSize = 26;
+
+  const { ref: expertiseRef, inView: isExpertiseVisible } = useInView({
+    threshold: 0.7,
+    triggerOnce: true,
+  });
+  const { ref: frameworksRef, inView: isFrameworksVisible } = useInView({
+    threshold: 0.7,
+    triggerOnce: true,
+  });
+  const { ref: dbRef, inView: isDbVisible } = useInView({
+    threshold: 0.7,
+    triggerOnce: true,
+  });
+
   return (
     <div className="container-fluid mx-auto h-screen p-5 md:p-10">
-      <div className="font-poppins text-4xl md:text-6xl text-center font-bold text-darkBlack">
+      <div className="font-poppins text-4xl md:text-6xl text-center font-bold text-primary">
         Skills
       </div>
       <div className="flex flex-col sm:flex-col md:flex-row md:justify-evenly mt-10 md:mt-20">
         <div className="w-full md:w-3/5">
-          <div className="mb-7">
-            <div className="font-poppins text-2xl text-black font-bold mb-2">
-              Expertise
+          <div className="mb-7" ref={expertiseRef}>
+            <div
+              className={`font-poppins text-black font-bold mb-2 text-2xl relative w-fit opacity-0 ${
+                isExpertiseVisible ? styles.textAnimation + " opacity-100" : ""
+              }`}
+            >
+              <span className={isExpertiseVisible ? styles.fadeIn : ""}>
+                Expertise
+              </span>
             </div>
-            <div className="flex gap-x-5 gap-y-5 flex-wrap justify-center md:justify-start text-primary">
-              <SkillBox
-                name="React JS"
-                icon={<SiReact size={iconSize} />}
-                rating={10}
-              />
-              <SkillBox
-                name="React Native"
-                icon={<SiReact size={iconSize} />}
-                rating={7}
-              />
-              <SkillBox
-                name="Node JS"
-                icon={<SiNodedotjs size={iconSize} />}
-                rating={7}
-              />
-              <SkillBox
-                name="TypeScript"
-                icon={<SiTypescript size={iconSize} />}
-                rating={6}
-              />
-              <SkillBox
-                name="HTML"
-                icon={<SiHtml5 size={iconSize} />}
-                rating={7}
-              />
-              <SkillBox
-                name="CSS"
-                icon={<SiCss3 size={iconSize} />}
-                rating={7}
-              />
-            </div>
-          </div>
-
-          <div className="mb-7">
-            <div className="font-poppins text-2xl text-black font-bold mb-2">
-              Frameworks
-            </div>
-            <div className="flex gap-x-5 gap-y-5 flex-wrap justify-center md:justify-start text-primary">
-              <SkillBox
-                name="Express JS"
-                icon={<SiExpress size={iconSize} />}
-                rating={7}
-              />
-              <SkillBox
-                name="Nest JS"
-                icon={<SiNestjs size={iconSize} />}
-                rating={7}
-              />
-              <SkillBox
-                name="Next JS"
-                icon={<SiNextdotjs size={iconSize} />}
-                rating={7}
-              />
-              <SkillBox
-                name="Material UI"
-                icon={<SiMaterialui size={iconSize} />}
-                rating={6}
-              />
+            <div className="flex justify-center md:justify-start">
+              <div
+                className={`flex gap-x-5 gap-y-5 flex-wrap text-primary opacity-0 transition-all duration-[1s] delay-[100ms] translate-y-10 ${
+                  isExpertiseVisible ? "translate-y-0 opacity-100" : ""
+                }`}
+              >
+                <SkillBox
+                  name="React JS"
+                  icon={<SiReact size={iconSize} />}
+                  rating={10}
+                />
+                <SkillBox
+                  name="React Native"
+                  icon={<SiReact size={iconSize} />}
+                  rating={7}
+                />
+                <SkillBox
+                  name="Node JS"
+                  icon={<SiNodedotjs size={iconSize} />}
+                  rating={7}
+                />
+                <SkillBox
+                  name="TypeScript"
+                  icon={<SiTypescript size={iconSize} />}
+                  rating={6}
+                />
+                <SkillBox
+                  name="HTML"
+                  icon={<SiHtml5 size={iconSize} />}
+                  rating={7}
+                />
+                <SkillBox
+                  name="CSS"
+                  icon={<SiCss3 size={iconSize} />}
+                  rating={7}
+                />
+              </div>
             </div>
           </div>
 
-          <div className="mb-7">
-            <div className="font-poppins text-2xl text-black font-bold mb-2">
-              Databases
+          <div className="mb-7" ref={frameworksRef}>
+            <div
+              className={`font-poppins text-2xl text-black font-bold mb-2 relative w-fit opacity-0
+                ${
+                  isFrameworksVisible
+                    ? styles.textAnimation + " opacity-100"
+                    : ""
+                }`}
+            >
+              <span className={isFrameworksVisible ? styles.fadeIn : ""}>
+                Frameworks
+              </span>
             </div>
-            <div className="flex gap-x-5 gap-y-5 flex-wrap justify-center md:justify-start text-primary">
-              <SkillBox
-                name="MongoDB"
-                icon={<SiMongodb size={iconSize} />}
-                rating={7}
-              />
-              <SkillBox
-                name="MySql"
-                icon={<SiMysql size={iconSize} />}
-                rating={6}
-              />
+            <div className="flex justify-center md:justify-start">
+              <div
+                className={`flex gap-x-5 gap-y-5 flex-wrap text-primary opacity-0 transition-all duration-[1s] delay-[100ms] translate-y-10 ${
+                  isFrameworksVisible ? "translate-y-0 opacity-100" : ""
+                }`}
+              >
+                <SkillBox
+                  name="Express JS"
+                  icon={<SiExpress size={iconSize} />}
+                  rating={7}
+                />
+                <SkillBox
+                  name="Nest JS"
+                  icon={<SiNestjs size={iconSize} />}
+                  rating={7}
+                />
+                <SkillBox
+                  name="Next JS"
+                  icon={<SiNextdotjs size={iconSize} />}
+                  rating={7}
+                />
+                <SkillBox
+                  name="Material UI"
+                  icon={<SiMaterialui size={iconSize} />}
+                  rating={6}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-7" ref={dbRef}>
+            <div
+              className={`font-poppins text-2xl text-black font-bold w-fit relative opacity-0 mb-2 ${
+                isDbVisible ? styles.textAnimation + " opacity-100" : ""
+              }`}
+            >
+              <span className={isDbVisible ? styles.fadeIn : ""}>
+                Databases
+              </span>
+            </div>
+            <div className="flex justify-center md:justify-start">
+              <div
+                className={`flex gap-x-5 gap-y-5 w-full flex-wrap text-primary opacity-0 transition-all duration-[1s] delay-[100ms] translate-y-10 ${
+                  isDbVisible ? "translate-y-0 opacity-100" : ""
+                }`}
+              >
+                <SkillBox
+                  name="MongoDB"
+                  icon={<SiMongodb size={iconSize} />}
+                  rating={7}
+                />
+                <SkillBox
+                  name="MySql"
+                  icon={<SiMysql size={iconSize} />}
+                  rating={6}
+                />
+              </div>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,43 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // prettier-ignore
+      keyframes: {
+        textBarKeyFrames: {
+          '0%': {
+            width: "0%",
+            left: 0,
+          },
+
+          '50%': {
+            width: "100%",
+            left: 0,
+          },
+
+          '100%': {
+            width: "0%",
+            left: "100%",
+          },
+        },
+
+        fadeIn: {
+          '0%': {
+            opacity: 0
+          },
+          '100%': {
+            opacity: 1
+          }
+        }
+      },
+      animation: {
+        beforetextBar:
+          "textBarKeyFrames 0.5s cubic-bezier(.74, .06, .4, .92) forwards",
+        aftertextBar:
+          "textBarKeyFrames 0.5s cubic-bezier(.74, .06, .4, .92) 0.25s forwards",
+        fadeIn: "fadeIn 1s 0.5s forwards",
+      },
+    },
     // prettier-ignore
     screens: {
       'sm': "368px",
@@ -24,6 +60,7 @@ module.exports = {
       bgSecondary: "#FCF7FF",
       primary: "#6C63FF",
       primaryDark: "#3c378a",
+      secondary: "#B3A7DD",
       darkBlack: "#050505",
       black: "#2F4858",
       white: "#ffffff",
